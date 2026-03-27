f34284ad7722ac066c054222fff2f833fa212b43
Blarglenarky <blarglenarky@gmail.com>
2026-03-26 20:13:11 -0500

    formatted buttons properly on landingPage

--- app/page.js
+++ app/page.js
@@ -4,20 +4,11 @@ export default function Home() {
   return (
     <main className="menu-page">
       <header className="menu-header">
-        <Link className="employee-login" href="/login">
-          Customer Login
-        </Link>
+        <Link className="employee-login" href="/login">Customer Login</Link>
+        <Link className="employee-login" href="/login">Employee Login</Link>
+        <Link className="employee-login" href="/login">Manager Login</Link>
       </header>
-      <header>
-        <Link className="employee-login" href="/login">
-          Cashier Login
-        </Link>
-      </header>
-      <header>
-        <Link className="employee-login" href="/login">
-          Manager Login
-        </Link>
-      </header>
+      
 
       <section className="menu-hero" aria-labelledby="menu-title">
         <p className="menu-label"></p>
