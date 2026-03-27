cf13d2a8669477c8d538ee10c1345e8a34c3ebef
Rohak <rohakb2004@gmail.com>
2026-03-26 21:18:59 -0500

    Modified landing page

--- app/page.js
+++ app/page.js
@@ -8,12 +8,17 @@ export default function Home() {
         <Link className="employee-login" href="/login?role=employee">Employee Login</Link>
         <Link className="employee-login" href="/login?role=manager">Manager Login</Link>
       </header>
-      
 
       <section className="menu-hero" aria-labelledby="menu-title">
         <p className="menu-label"></p>
         <h1 id="menu-title">Welcome!<br></br>Page in progress!</h1>
       </section>
+
+      <section className="menu-img">
+        <img src="/image.png"></img>  
+        <img src="/image.png"></img> 
+        <img src="/image.png"></img>
+      </section> 
 
       <section className = "landingPageBottomSection">
         <h4 className = "landingPageBottomHeader">These r examples buttons that will link to other pages. Implementation needs fixing later:</h4>
