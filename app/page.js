35b5a630fd3123a32b41391634e2eb87ad29c418
Rohak <rohakb2004@gmail.com>
2026-03-26 22:40:48 -0500

    one button

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <main className="menu-page">
       <header className="menu-header">
-        <Link className="employee-login" href="/login?role=customer">Customer Login</Link>
-        <Link className="employee-login" href="/login?role=employee">Employee Login</Link>
-        <Link className="employee-login" href="/login?role=manager">Manager Login</Link>
+        <Link className="employee-login" href="/login?role=customer">Login</Link>
       </header>
 
       <section className="menu-hero" aria-labelledby="menu-title">
