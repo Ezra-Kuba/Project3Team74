80bdf968cfcf220b446b6e82f630fe481db4955e
Blarglenarky <blarglenarky@gmail.com>
2026-03-26 19:59:58 -0500

    Added buttons (unformatted) for manager and customer login

--- app/page.js
+++ app/page.js
@@ -5,13 +5,23 @@ export default function Home() {
     <main className="menu-page">
       <header className="menu-header">
         <Link className="employee-login" href="/login">
-          Employee Login
+          Customer Login
+        </Link>
+      </header>
+      <header>
+        <Link className="employee-login" href="/login">
+          Cashier Login
+        </Link>
+      </header>
+      <header>
+        <Link className="employee-login" href="/login">
+          Manager Login
         </Link>
       </header>
 
       <section className="menu-hero" aria-labelledby="menu-title">
         <p className="menu-label"></p>
-        <h1 id="menu-title">Self-Serve Customer Functionality Here this is our portal page, We need this i think</h1>
+        <h1 id="menu-title">Self-Serve Customer Functionality Here this is our portal page, We need this i</h1>
       </section>
     </main>
   );
