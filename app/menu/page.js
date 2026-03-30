c8e919797e5c448dcb5f0a06aaab2d8df40b3947
Rohak <rohakb2004@gmail.com>
2026-03-30 04:34:59 -0500

    added navbar to menu page so you can navigate out of it without hit the back arrow on ur browser for convenience to the user and optimization

--- app/menu/page.js
+++ app/menu/page.js
@@ -5,6 +5,17 @@ export const metadata = {
 export default function MenuGUI() {
   return (
     <main className="menu-page">
+      <nav class="navbar">
+        <div class="logo">Supa Yummi Boba</div>
+        <ul class="nav-links">
+          <li><a className="nav-bar-items" href="/">Home</a></li>
+          <li><a className="nav-bar-items" href="#about">About</a></li>
+          <li><a className="nav-bar-items" href="/">Menu</a></li>
+          <li><a className="nav-bar-items" href="#contact">Contact</a></li>
+          <li><a className="nav-bar-items" href="/login">Login</a></li>
+        </ul>
+      </nav>
+      
       <section className="menu-title" aria-labelledby="menu-title">
         <h1 id="menu-title">Menu Page</h1>
         <p className="menu-copy">This page is not done, but this is where we will begin implementing a menu page. <br></br>That way we can just link it to other pages when the login ect. features are done.</p>
