ff5c8a8f379522a8ae6f2a485be2c1e373705cac
Ezra G Kuba <ezragrey05@gmail.com>
2026-04-02 12:56:08 -0500

    added final GUI stuff

--- app/about/page.js
+++ app/about/page.js
@@ -12,10 +12,19 @@ export default function AboutPage() {
           <li><a className="nav-bar-items" href="/login">Login</a></li>
         </ul>
     </nav>
-    <section className="image and paragraph">
+    <section className="about-body">
         <h1>About us</h1>
         <img src="/jesse.png"></img>
-        <p>this is a fake biography about founder Jesse Ashen.</p>
+        <img src="chineseJesse.png"></img>
+        <p>The story of Supa Yummi Boba is one of great trials and tribulations.
+          Before getting into the Boba Tea business, our founder Jesse Ashen was a 
+          sad lonely man. He only lived for food one could only describe as "goyslop"
+          and Roblox. One day, however, an ancient Chinese Water Lemur came to him in a dream.
+          He told him "my friend, this is not your path. I will make you into who you really are
+          , then you will find your destiny." When Jesse awoke, he was now a changed man, fully
+          embracing his Chinese roots. From then on, he knew his calling was Boba Tea, and the rest is
+          history.
+        </p>
     </section>
     </main>
   );
